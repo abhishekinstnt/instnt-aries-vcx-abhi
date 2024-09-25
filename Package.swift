14ae6faa3dcd01e38aa952762ad04f87aeae3962
abhishek <abhishek@instnt.org>
2024-09-25 20:40:41 +0530

    updating package file

--- Package.swift
+++ Package.swift
@@ -7,22 +7,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "InstntAriesVCXPackage",
+    name: "InstntAriesVCX",
     products: [
         .library(
-            name: "InstntAriesVCXPackage",
-            targets: ["InstntAriesVCXPackage"]),
+            name: "InstntAriesVCX",
+            targets: ["InstntAriesVCX"]),
     ],
     targets: [
         .target(
-            name: "InstntAriesVCXPackage",
-            dependencies: ["VCX_uniffiFFI_Lib"],
+            name: "InstntAriesVCX",
+            // dependencies: ["VCX_uniffiFFI_Lib"],
             path: "aries/agents/ios/ariesvcx/ariesvcx/Source/"
             ),
-        .binaryTarget(
-            name: "VCX_uniffiFFI_Lib",
-            url: "https://github.com/instnt-inc/instnt-aries-vcx/releases/download/abhishek_GithubAction2/vcx.xcframework.zip",
-            checksum: "c8964e8a20c2e74199cf40848b7b14afc95a4f3dcae80a4dba974acf54d253ac"
-        )
+        // .binaryTarget(
+        //     name: "VCX_uniffiFFI_Lib",
+        //     url: "https://github.com/instnt-inc/instnt-aries-vcx/releases/download/abhishek_GithubAction2/vcx.xcframework.zip",
+        //     checksum: "c8964e8a20c2e74199cf40848b7b14afc95a4f3dcae80a4dba974acf54d253ac"
+        // )
     ]
 )
+
+let InstntAriesVCXTarget = package.targets.first(where: { $0.name == "InstntAriesVCX" })
+
+package.targets.append(.binaryTarget(
+        name: "VCX_uniffiFFI_Lib_zip",
+        path: url: "https://github.com/instnt-inc/instnt-aries-vcx/releases/download/abhishek_GithubAction2/vcx.xcframework.zip",
+        checksum: "c8964e8a20c2e74199cf40848b7b14afc95a4f3dcae80a4dba974acf54d253ac"
+        ))
+
+InstntAriesVCXTarget?.dependencies.append("VCX_uniffiFFI_Lib_zip")
